0e8dc84286b4b7c5d35d3ac753bfc59161948134
Alex Lindsay <alexlindsay239@gmail.com>
2020-09-17 16:28:24 -0700

    INSFV
    Includes:
    - lid-driven examples
    - channel-flow examples
    - RZ examples

---
+++ modules/navier_stokes/include/fv/bcs/FVPressurePin.h
@@ -0,0 +1,68 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "NodalBCBase.h"
+#include "MooseVariableInterface.h"
+
+template <typename>
+class MooseVariableFV;
+class FEProblemBase;
+class SystemBase;
+class MooseMesh;
+
+/**
+ * Constrain a single dof for the pressure variable
+ */
+class FVPressurePin final : public NodalBCBase, public MooseVariableInterface<Real>
+{
+public:
+  static InputParameters validParams();
+
+  FVPressurePin(const InputParameters & parameters);
+
+  void computeResidual() override;
+  void computeJacobian() override;
+  void computeOffDiagJacobian(unsigned int jvar) override;
+
+  MooseVariableFieldBase & variable() override { return _var; }
+
+  bool checkNodalVar() const override { return false; }
+
+private:
+  /**
+   * @return the index for the pressure variable on an element connected to the node
+   */
+  dof_id_type getDofIndex();
+
+  /**
+   * emit a boundary parameter error message telling the user we should only execute on a single
+   * node
+   */
+  void boundaryParamError();
+
+  /// the pressure variable
+  MooseVariableFV<Real> & _var;
+
+  /// Reference to FEProblemBase
+  FEProblemBase & _fe_problem;
+
+  /// Reference to SystemBase
+  SystemBase & _sys;
+
+  /// Mesh this BC is defined on
+  MooseMesh & _mesh;
+
+  /// The Dirichlet value
+  const Real & _value;
+
+  /// The only valid node_id that this object should be executed on
+  dof_id_type _valid_node;
+};
